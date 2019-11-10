37c37dd9b3efa61295036bef7d567f0ca96df538
Ishan_kansara <ishan.stealth@gmail.com>
2019-11-10 16:06:25 -0500

    Updated comments for all functions in File 1

--- Files_C/File_1/main.c
+++ Files_C/File_1/main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
-
+/**
+Description: This function opens the file, writes content into
+file, brings the cursor at the beginning of the file and counts the
+number of lines in txt file.
+@Param: *file
+@Return: void
+**/
 void openFile(char *file)
 {
     FILE *pFile;
@@ -14,7 +18,12 @@ void openFile(char *file)
     closeFile(pFile);
 }
 
-
+/**
+Description: This function writes content into file in txt file.
+It uses fputc functions.
+@Param: *wFile
+@Return: void
+**/
 void writeFile(FILE *wFile)
 {
     fputs("My name is Ishan.\n",wFile);
@@ -26,6 +35,12 @@ void writeFile(FILE *wFile)
 
 }
 
+/**
+Description: This function reads each character from file and counts
+number of total lines in txt file. It uses fgetc functions.
+@Param: *countFile
+@Return: void
+**/
 void countNumOfLines(FILE *countFile)
 {
     int count = 0;
@@ -41,6 +56,12 @@ void countNumOfLines(FILE *countFile)
     printf("Total count: %d", count+1);
 }
 
+/**
+Description: This function closes file and points it to NULL value.
+It uses fclose functions.
+@Param: *cFile
+@Return: void
+**/
 void closeFile(FILE *cFile)
 {
     fclose(cFile);
@@ -50,11 +71,6 @@ void closeFile(FILE *cFile)
 int main()
 {
     char *fileName = "Test_File.txt";
-    //pFile = fopen("Test_File.txt","w+");
-    //fputs("Hi my name is ishan.",pFile);
-    //fclose(pFile);
     openFile(fileName);
-    //writeFile(&pFile);
-    //closeFile(&pFile);
     return 0;
 }
